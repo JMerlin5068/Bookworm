4cddd21f8b65f6f7f10bc78073a9d49c16511384
Jarrod Merlin <jarrod.merlin@stu.barren.kyschools.us>
2024-04-22 08:30:37 -0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Student on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
